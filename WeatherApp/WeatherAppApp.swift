ade711d743fe1f3e1e64abd0bbf806ab8e6468ae
DaraDaraV <devdaradara@gmail.com>
2023-02-17 18:58:45 +0900

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by 류지예 on 2023/02/17.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
